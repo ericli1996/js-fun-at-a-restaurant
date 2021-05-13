8f59bda9d2a224ec15daddd8c81e124a76d54012
Eric Li <ericli@knights.ucf.edu>
2021-05-12 22:46:17 -0600

    Add Iteration 1

--- src/warm-up.js
+++ src/warm-up.js
@@ -8,23 +8,28 @@
 
 // Initialize three new variables, "food", "type" and "inStock", and assign values to them with
 // a food's name, type and number of items in stock.
-
 // var food = "Ice Cream";
 // var type = "Dessert";
 // var inStock = 10
+var food = "Dumplings";
+var type = "Dim Sum";
+var inStock = 22;
 
 
 
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
+inStock = inStock * 2;
 
 
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
+inStock = inStock - 1;
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
+var isFavorite = true;
 
 
 
@@ -32,16 +37,22 @@
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
-
+var shouldOrderMore = inStock < 20;
+console.log(shouldOrderMore);
 
 
 //------------------------- Conditionals -------------------------//
 
 
 
-// Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// Express the following in code: If the variable "inStock" is greater or equal to 1, log
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
+if (inStock >= 1) {
+  console.log("I'm going to eat some " + food + "!");
+} else {
+  console.log("We need to get some more " + food + ".");
+};
 
 
 
@@ -49,7 +60,11 @@
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
-
+if (isFavorite === true) {
+  console.log(`My favorite food is ${food}!`);
+} else {
+  console.log(`I recently ate ${food}.`);
+};
 
 
 //------------------------- Arrays -------------------------//
@@ -57,26 +72,36 @@
 
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
+var vegetables = ["zucchini", "squash", "cabbage", "leek", "scallion"];
 
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
+vegetables[2];
 
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
+var fruits = ["tomato", "durian", "lychee", "orange", "jackfruit", "apple", "tangerine"];
 
 
 // Write the code to access the 5th element of the array, using bracket notation.
+fruits[4];
 
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable called "groceryStore" and assign to it an object literal with two
-// keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
+// keys: name, and isOpen. The value for name should be the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
+var groceryStore = {
+  name: "Publix",
+  isOpen: true
+};
+
+// Other key value pairs that might make sense are location, hours, date founded, etc.
 
 
-
-// Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
+// Write the code to add the key of location, with the value of 'Orlando, Florida'. Use dot
 // notation to do this. DO NOT modify your code from above.
+groceryStore.location = "Orlando, Florida";
